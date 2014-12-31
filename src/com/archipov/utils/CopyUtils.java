26a3af1872731c67e6b1c8a6c6e4c85c56749e36
kirillvs <kirillvs85@mail.ru>
2014-12-31 17:03:54 +1000

    fox for static final field

--- src/com/archipov/utils/CopyUtils.java
+++ src/com/archipov/utils/CopyUtils.java
@@ -3,6 +3,7 @@ package com.archipov.utils;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
 
 public class CopyUtils {
 	
@@ -79,5 +80,17 @@ public class CopyUtils {
 			fields[i].setAccessible(true);
 			fields[i].set(finObj, fields[i].get(srcObj));
 		}
+	}
+	
+	
+	//Взято со Stackoverflow http://stackoverflow.com/questions/3301635/change-private-static-final-field-using-java-reflection
+	static void setFinalStaticField(Field field, Object newValue) throws Exception {
+		field.setAccessible(true);
+		
+		Field modifiersField = Field.class.getDeclaredField("modifiers");
+		modifiersField.setAccessible(true);
+		modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
+
+		field.set(null, newValue);
 	}
 }
