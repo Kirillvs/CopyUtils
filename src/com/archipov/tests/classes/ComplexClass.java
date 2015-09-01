09caaa9440d2ea8bbe371c6a394628030a25f0d0
kirillvs <kirillvs85@mail.ru>
2015-09-01 13:07:09 +1000

    update

---
+++ src/com/archipov/tests/classes/ComplexClass.java
@@ -0,0 +1,32 @@
+package com.archipov.tests.classes;
+
+public class ComplexClass {
+	
+	private double id;
+	SecondComplexObject sco;
+	
+	public ComplexClass() {
+		id = Math.random()*1000;
+		sco = new SecondComplexObject(this);
+	}
+	
+	public void printID(){
+		System.out.println("First obj - " + id);
+		sco.printID();
+	}
+	
+	public double getID(){
+		return this.id;
+	}
+	
+	public SecondComplexObject getChild(){
+		return sco;
+	}
+	
+	@Override
+	public String toString() {
+		Double idToString = new Double(id);
+		return idToString.toString();
+	}	
+
+}
---
+++ src/com/archipov/tests/classes/SecondComplexObject.java
@@ -0,0 +1,29 @@
+package com.archipov.tests.classes;
+
+public class SecondComplexObject {
+	
+	private ComplexClass parrentComplex;
+	private double id;
+
+	public SecondComplexObject(ComplexClass cs) {
+		parrentComplex = cs;
+		id = Math.random()*1000;		
+	}
+	
+	public String printID(){
+		System.out.println("Second obj - " + id + "l; ParrentFirst - " + parrentComplex.getID());
+		return "Second obj - " + id + "l; ParrentFirst - " + parrentComplex.getID();
+	}
+	
+	public double getID(){
+		return this.id;
+	}
+	/*
+	@Override
+	public String toString() {
+		Double idToString = new Double(id);
+		return idToString.toString();
+	}
+	*/
+
+}
